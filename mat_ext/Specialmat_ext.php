8c04040e6adb27468bcbedd008c7387ab35dbdf1
albertcoder <eralbert9191@gmail.com>
2014-07-12 02:20:09 +0530

    added option to delete a trait. Only those traits can be deleted which were added by that user.

--- mat_ext/Specialmat_ext.php
+++ mat_ext/Specialmat_ext.php
@@ -185,6 +185,7 @@ parent::__construct('mat_ext_one');
 }
 public function execute($sub){
 	global $wgUser;
+$name=$wgUser->getId();
 if($wgUser->isLoggedIn()){
 	global $wgOut;
 	global $array;
@@ -206,6 +207,7 @@ $g++;
 if(isset($_POST['addtrait'])){
 $r=array('id'=>0,
 'trait_name'=>str_ireplace(" ", "_", strtolower($_POST['trait_name'])),
+'userID'=>$name,
 't_type'=>$_POST['trait_type'],
 'u_type'=>$_POST['units']);
 $strtolower = str_ireplace(" ", "_", strtolower($_POST['trait_name']));
@@ -254,4 +256,49 @@ else
 }
 }
 }
+class Specialmat_ext_del extends SpecialPage{
+public function __construct(){
+parent::__construct('mat_ext_del');
+}
+public function execute($sub){
+	global $wgUser;
+$name=$wgUser->getId();
+if($wgUser->isLoggedIn()){
+	global $wgOut;
+	global $array;
+	global $wgDBprefix;
+	$dbr=wfGetDB(DB_SLAVE);
+	$wiki_message = 'J3';
+	$wgOut->addWikiMsg('add_trait');
+	$this->getOutput()->setPageTitle( 'Delete Trait' );
+	$dbw = wfGetDB( DB_MASTER );	
+$this->getOutput()->addHTML("<form action='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_del' method='post'>
+<table>");
+$this->getOutput()->addHTML("
+<tr><td>Select Trait to delete</td><td><select required name='traitsdel'>");
+$resdel=$dbr->select('trait_table',array('id','trait_name'),"userID=$name",__METHOD__);
+foreach($resdel as $utype){
+$this->getOutput()->addHTML("<option value=".$utype->trait_name.">".$utype->trait_name."</option>");
+}
+$this->getOutput()->addHTML("</select></td></tr>
+<tr><td><input type='submit' value='Delete' name='del' ></td></tr></table></form>");
+if(count($resdel)!=0){
 
+echo "ggg";
+if(isset($_POST['del'])){
+$dbw->query("DROP TABLE wiki_".$_POST['traitsdel']."");
+$dbw->query("DELETE FROM `wiki_trait_table` WHERE `trait_name` ='".$_POST['traitsdel']."'");
+}}
+else{
+echo "Sorry You don't have any trait in your trait-list to delete";
+}
+}
+ //LogIn
+
+
+else
+{
+        $this->getOutput()->addHTML("<h3 style='color:red'>Please <a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."?title=Special:UserLogin&returnto=Special%3AMat+ext'>Login</a> to add new Data</h3>");
+}
+
+}}
