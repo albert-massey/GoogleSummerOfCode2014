20df873a763c12ff76e8d9bc55cf0eafc4a9f238
albertcoder <eralbert9191@gmail.com>
2014-07-25 17:12:03 +0530

    Made search easier using dropdown for search

--- mat_ext/Specialmat_ext.php
+++ mat_ext/Specialmat_ext.php
@@ -432,7 +432,7 @@ if($wgUser->isLoggedIn()){
 $this->getOutput()->addHTML("<form action='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_delm' method='post'>
 <table>");
 $this->getOutput()->addHTML("
-<tr><td>Select Trait to delete</td><td><select required name='materialdel'>");
+<tr><td>Select Material to delete</td><td><select required name='materialdel'>");
 $matdel=$dbr->select('material',array('material_name'),"userID=$name",__METHOD__);
 foreach($matdel as $utype){
 $this->getOutput()->addHTML("<option value=".$utype->material_name.">".$utype->material_name."</option>");
@@ -691,15 +691,39 @@ public function execute($sub){
                     </body>
                     ");
 
+/*$this->getOutput()->addHTML("<form action='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_delm' method='post'>
+<table>");
+$this->getOutput()->addHTML("
+<tr><td>Select Trait to delete</td><td><select required name='materialdel'>");
+$matdel=$dbr->select('material',array('material_name'),"userID=$name",__METHOD__);
+foreach($matdel as $utype){
+$this->getOutput()->addHTML("<option value=".$utype->material_name.">".$utype->material_name."</option>");
+}
+$this->getOutput()->addHTML("</select></td></tr>
+<tr><td><input type='submit' value='Delete' name='del' ></td></tr></table></form>");
 
+if(isset($_POST['del'])){
+$dbw->query("DELETE FROM `wiki_material` WHERE `material_name` ='".$_POST['materialdel']."'");
+$page = $_SERVER['PHP_SELF'];
+header( "refresh: 0; url=$page" );
+}
+else{
+$this->getOutput()->addHTML("<h4 style='color:#FF0000'>Sorry You don't have any material in your material-list to delete</h4>");
+}
+ */
 
- $this->getOutput()->addHTML("<form action=http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:Mat_ext_searchm method='post'><table>
- <tr><td>Search by Material Name</td>
- <td><input type='text' name='t1'></td></tr>
- <tr><td><input type='submit' name='sb' value='Search'></td></tr>
- </table></form>");
- if(isset($_POST['t1'])){
- $res3=$dbr->select('material',array('mat_type,id'),"material_name='".$_POST['t1']."'",__METHOD__);
+$this->getOutput()->addHTML("<form action='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_searchm' method='post'>
+<table>");
+$this->getOutput()->addHTML("
+<tr><td>Select Trait to delete</td><td><select required name='materialdel'>");
+$matdel=$dbr->select('material',array('material_name'),"",__METHOD__);
+foreach($matdel as $utype){
+$this->getOutput()->addHTML("<option  value=".$utype->material_name.">".$utype->material_name."</option>");
+}
+$this->getOutput()->addHTML("</select></td></tr>
+<tr><td><input type='submit' value='Search' name='searchm' ></td></tr></table></form>");
+ if(isset($_POST['searchm'])){
+ $res3=$dbr->select('material',array('mat_type,id'),"material_name='".$_POST['materialdel']."'",__METHOD__);
  foreach($res3 as $d){
  $r[0]=$d->mat_type;
  $r[1]=$d->id;
@@ -737,3 +761,102 @@ public function execute($sub){
 }
 }
 }
+
+
+
+class Specialmat_ext_update extends SpecialPage{
+public function __construct(){
+parent::__construct('mat_ext_update');
+}
+public function execute($sub){
+global $wgOut;
+global $array;
+global $wgUser;
+global $count;
+	global $wgDBprefix;
+$name=$wgUser->getId();
+$dbr=wfGetDB(DB_SLAVE);
+	$dbw = wfGetDB( DB_MASTER );	
+$this->getOutput()->setPageTitle( 'Materials Database Extension' );
+if($wgUser->isLoggedIn()){
+	/**This code makes the menu bar at the top of each page */
+
+	$this->getOutput()->addHTML("<style>
+#menu ul{
+  list-style: none;
+  }
+
+  #menu li{
+    display: inline;
+    }
+    </style>
+    <div id='menu'>
+
+
+
+    <body>
+      <ul>
+          <li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext'><img style='border:0;' src='smiley.gif' alt='Add new material' width='42' height='42' title='add new material'></a></li>
+	  &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_one'><img style='border:0;' src='smiley.gif' alt='Add new trait' width='42' height='42' title='add new trait'></a></li>
+         &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_delm'><img style='border:0;' src='smiley.gif' alt='Delete material' width='42' height='42' title='delete material'></a></li>
+        &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_del'><img style='border:0;' src='smiley.gif' alt='Delete trait' width='42' height='42' title='delete trait'></a></li>
+        &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_viewall'><img style='border:0;' src='smiley.gif' alt='View all materials' width='42' height='42' title='view all materials'></a></li>
+         &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_searcht'><img style='border:0;' src='smiley.gif' alt='Search by TRAIT' width='42' height='42' title='search by TRAIT'></a></li>
+         &nbsp;<li><a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_searchm'><img style='border:0;' src='smiley.gif' alt='Search by MATERIAL' width='42' height='42' title='search by MATERIAL'></a></li>
+                    </ul>
+                    </div>
+
+
+                    </body>
+                    ");
+
+
+/** This code used for create  data entering form */
+$this->getOutput()->setPageTitle( 'Add New Material' );
+$this->getOutput()->addHTML("<form action='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."/Special:mat_ext_update' method='post'>
+<table><tr><td>Material Name</td><td><input required type='text' title='Example: Carbon'  name='t1'></td></tr>
+<tr><td>Trait Name</td><td><input required type='text' name='t2'></td></tr>
+<tr><td>Value</td><td><input required type='text' name='t3'></td>
+</table>");
+
+
+if(isset($_POST['t2'])){
+	//echo $_POST['t3'];
+/*$mat_id=$dbw->query("Select id from ".$wgDBprefix."material where material_name='".$_POST['t1']."'");
+//echo $mat_id->current();
+$mat_ids=(string)$mat_id;
+$dbw->query(" UPDATE ".$wgDBprefix.$_POST['t2']." 
+	SET value=".$_POST['t3']."
+	WHERE mat_id=$mat_ids;");
+*/
+//$mat_id=$dbr->select('material','id',"material_name=$_POST['t1']",__METHOD__);
+$mat_id=$dbw->query("Select id from ".$wgDBprefix."material where material_name='".$_POST['t1']."'");
+$id=0;
+foreach($mat_id as $f){
+foreach($f as $t){
+$id=$t; /** get maximum value of ID */
+}
+}
+$dbw->query(" UPDATE ".$wgDBprefix.$_POST['t2']." 
+	SET value=".$_POST['t3']."
+	WHERE mat_id=$id;");
+
+
+}
+
+
+}
+
+
+
+/** End of insertion code */
+/** This code makes dynamic traits for material */
+$res=$dbr->select('trait_table',array('trait_name','id'),"",__METHOD__);
+$v=0;
+$this->getOutput()->addHTML("<table>");
+$this->getOutput()->addHTML("<tr><td><input type='submit' value='Add' name='add' ></td></tr></table></form>");
+
+
+     }
+     }
+
