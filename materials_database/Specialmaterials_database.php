abe40c603cfe3f2202a7bba9fd6271e3b9cc158f
AlbertCoder <eralbert9191@gmail.com>
2015-03-31 15:09:24 +0530

    removes the data being shown without login

--- materials_database/Specialmaterials_database.php
+++ materials_database/Specialmaterials_database.php
@@ -172,26 +172,7 @@ class Specialmaterials_database extends SpecialPage {
 		}
 	    }
 	    else {
-		$this->getOutput()->addHTML("<h3 style='color:black'>Please <a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."?title=Special:UserLogin&returnto=Special%3AMaterials+database'>Login</a> to add new Data</h3>");
-		$res2 = $dbr->select('trait_table',array('trait_name'),"",__METHOD__);
-		$g = 0;
-		foreach ($res2 as $samedata) {
-		    $array[$g] = $samedata->trait_name;
-		    $count = $g + 1;
-		    $g++;
-		}
-		for ($i = 0; $i < $count; $i++) {
-		    $res = $dbr->select(
-		    array('material',$array[$i]),
-		    array('material_name','value',"{$dbr->tableName( $array[$i] )}.timestamp" ),
-		    array('mat_id>0'),__METHOD__,array(),
-		    array($array[$i] => array('INNER JOIN', array("{$dbr->tableName( 'material' )}.id=mat_id"))));
-		    $this->getOutput()->addHTML("<table border='1' width='550' height='30' cellspacing='1' cellpadding='3'><tr><th>Material Name</th><th>".ucwords(str_ireplace("_", " ", $array[$i]))."</th><th>Timestamp</th></tr>");
-		    foreach ($res as $row) {
-			$this->getOutput()->addHTML("<tr><td>".$row->material_name."</td><td>".$row->value."</td><td>".$row->timestamp."</td></tr>");
-		    }
-		    $this->getOutput()->addHTML("</table><br>");
-		}
+		$this->getOutput()->addHTML("<h3 style='color:black'>Please <a href='http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."?title=Special:UserLogin&returnto=Special%3AMaterials+database'>Login</a> to continue</h3>");
 	    }
 	}
     }
